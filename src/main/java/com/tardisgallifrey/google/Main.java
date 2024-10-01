39767a97c546f19f5443033a4f3b239d8250836b
tardisgallifrey <bigmanrunning@gmail.com>
2024-10-01 13:59:44 -0500

    first commit 10/1/24

---
+++ src/main/java/com/tardisgallifrey/google/Main.java
@@ -0,0 +1,27 @@
+package com.tardisgallifrey.google;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int key = 63;
+        int H1 = 10;
+        int H2 = 11;
+        int i = 2;
+
+
+        System.out.println(((hash1(key) + i * hash2(key)) % H2));
+
+
+    }
+
+    public static int hash1(int key){
+        int H1 = 11;
+        return key % H1;
+    }
+
+    public static int hash2(int key){
+        int H2 = 5;
+        return H2 - key % H2;
+    }
+}
